d44c329cf099d47a1319f13eb627ffc5ec3e9ac6
Shewangzaw Gebeyehu <shewangzaw.gebeyehu@gmail.com>
2019-12-27 17:58:29 -0500

    second commit

---
+++ mnalesh-angular/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './helpers/auth.guard';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
